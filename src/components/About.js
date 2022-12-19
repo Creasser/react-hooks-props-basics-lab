a1496b0983050682092d2b050c4986e640b12041
Blake Creasser <blakecreasser@aggienetwork.com>
2022-12-19 14:46:23 -0600

    commit tree 540d43584763d1ad76bd6f781cf2bb25208ee381

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Links from '../components/Links'
 
-function About() {
+
+function About(props) {
+  function returnBio(){
+    if (props.bio === '' || props.bio === undefined){
+      return null
+    }else{
+      return <p>{props.bio}</p>
+    }
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {returnBio()}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links 
+      github={props.github}
+      linkedin={props.linkedin}
+      />
     </div>
   );
 }
+
+
 
 export default About;
